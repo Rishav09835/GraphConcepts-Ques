8ead247462c63eb918342a845bfe55c86b6670ae
Rishav Dobriyal <73049840+Rishav09835@users.noreply.github.com>
2025-04-27 13:09:09 +0530

    Update UF_Path&rank.cpp

--- DSU_UnionFind/UF_Path&rank.cpp
+++ DSU_UnionFind/UF_Path&rank.cpp
@@ -1,6 +1,26 @@
 vector<int>parent;
 vector<int>rank;
 
-int find(int x, parent){
-  
+int find(int x){
+  if(parent[x]==x) return x;
+  return parent[x]=find(parent[x]);
+}
+
+void Union(int x, int y)
+{
+  int parentx = find(x);
+  int parenty = find(y);
+  if(parentx == parenty) return;
+  if(rank[parentx] > rank[parenty]) parent[parenty] = parentx;
+  else if(rank[parentx] < rank[parenty]) parent[parentx] = parenty;
+  else {
+    rank[parentx] = parenty;
+    parent[parenty]++;
+      }
+}
+
+int main(){
+  parent.resize(n);
+  rank.resize(n);
+  return 0;
 }
