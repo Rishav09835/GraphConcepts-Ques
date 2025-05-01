aa907c62d9a3d4c69cd57c6c5a17a9d708bbbc2a
Rishav Dobriyal <73049840+Rishav09835@users.noreply.github.com>
2025-05-01 23:11:28 +0530

    Update Using_BFS.cpp

--- Topological_sort/Using_BFS.cpp
+++ Topological_sort/Using_BFS.cpp
@@ -1,1 +1,9 @@
-Topo_BFS(vector<bool>inRecurs, vector<int>)
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+  vector<vector<int>> adj(v);
+  for(auto &e : edges){
+    int u = e[0];
+    int v = e[1];
+    adj[v].push_back(u);
+  }
+}
